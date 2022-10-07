94b35710fd1fa4e162dacf7e383dc98bfc2bf310
DONIHSAN <115215024+DONIHSAN@users.noreply.github.com>
2022-10-07 14:52:03 +0700

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,26 @@
+# python
+# membuat a string
+# dengan single quotes
+
+from pickletools import string1
+from tokenize import String
+
+
+string1 = "walau membiru"
+print("bisakah terus mengobati: ")
+print(string1)
+# membuat string
+# dengan double quotes
+string1 = "ketemu malu ga ketemu rindu"
+print("\nsebab tujuan mencintai adalah merelakan pergi: ")
+print(string1)
+# membuat string
+# dengan tiga qoutes
+string1 ="aku tidak pandai bercerita""tapi aku pendengar yang sangat baik"
+print("\nKata dari bidadari: ")
+print(string1)
+# creating string with triple
+# quotes allows multiple lines
+string1 = "saling mendahului"
+print("\nHidup bukan untuk : ")
+print(string1)
